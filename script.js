589c1d1bd62125cc74d8e34915278a8ad04167e8
Vlad Sazonov <vladsazon27@gmail.com>
2017-03-11 11:16:43 +0300

    Todo

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+$('#btn').on('click', function() {
+  $('#btn').on('click', function(event) {
+    $('#submit').val("")
+  })
+
+  var entrada = $('#submit').val();
+  if ($("#submit").val() == '') {
+    return false;
+  } 
+var RemoveListItem = function(src)
+{
+ 
+};
+  $('#list').prepend('<li>' + entrada + '</li>');
+  $('#list li').click(RemoveListItem); 
+ localStorage.setItem("todoist", $('#list').html());
+  $('#submit').val("");
+  $('#list').listview('refresh');
+})
+
+$(function(){
+  $('#list').html(localStorage.getItem("todoist"));
+
+  $(document).on('click', 'li', function(){
+   $(this).remove();
+  
+   localStorage.removeItem("todoist");
+ }); 
+});
+  // localStorage.removeItem("todoist"); 
+})(); 
